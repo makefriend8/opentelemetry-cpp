ea5dff27569a5b3af613bf09a2e02aa685c495e2
MarsZhang <6671605@gmail.com>
2021-11-29 09:56:42 +0800

    Update mybuild.sh

--- mybuild.sh
+++ mybuild.sh
@@ -2,9 +2,10 @@
  git clone https://github.com/google/benchmark.git
  cd benchmark
  cmake -E make_directory "build"
- cmake -E chdir "build" cmake -DBENCHMARK_DOWNLOAD_DEPENDENCIES=on -DCMAKE_BUILD_TYPE=Release ../
+ cmake -E chdir "build" cmake -DBENCHMARK_DOWNLOAD_DEPENDENCIES=off -DCMAKE_BUILD_TYPE=Release ../
  cmake --build "build" --config Release
  cd ..
+ cp -r benchmark/build/third_party/googletest benchmark/
  mkdir build
  cd build
  cmake ..
